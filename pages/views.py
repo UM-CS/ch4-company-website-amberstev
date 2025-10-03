052988f3bc158a61767bb468b89f545c421a74d7
Amber <amberstevenson04@gmail.com>
2025-10-02 19:34:09 -0600

    commit 2

--- pages/views.py
+++ pages/views.py
@@ -16,3 +16,11 @@ class AboutPageView(TemplateView):
         context["contact_address"] = "123 Main Street"
         context ["phone_number"] = "555-555-5555"
         return context
+    
+class ProductsPageView(TemplateView):
+    template_name = "products.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_list"] = ["Product A", "Product B", "Product C", "Product D"]
+        return context
